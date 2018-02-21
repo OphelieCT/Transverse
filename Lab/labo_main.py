807fcdc6e535603be18b2e74818483f5526e239d
0rigine <malo.puissant.20160347@efrei.net>
2018-02-21 18:30:10 +0100

    Lab package init

--- Lab/labo_main.py
+++ Lab/labo_main.py
@@ -5,6 +5,10 @@
 """ Lab main script of project """
 
 # ---- Imports ----
+import keras.backend as K
+from keras.models import Model
+from keras.layers import Dense
+from keras.applications import VGG16
 
 # ---- Script ----
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# ---- Description ----
+""" Description """
+
+# ---- Imports ----
+
+# ---- Script ----
