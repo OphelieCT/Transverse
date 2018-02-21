223b11e68c63ad5b4e3647003fed5f3c4a50c8c3
0rigine <malo.puissant.20160347@efrei.net>
2018-02-21 18:38:13 +0100

    Lab init

--- Lab/labo_main.py
+++ Lab/labo_main.py
@@ -5,15 +5,8 @@
 """ Lab main script of project """
 
 # ---- Imports ----
-import keras.backend as K
-from keras.models import Model
-from keras.layers import Dense
-from keras.applications import VGG16
-from keras.preprocessing.image import img_to_array, ImageDataGenerator
-from PIL.Image import Image
+
 
 # ---- Script ----
 if __name__ == '__main__':
-    datagen = ImageDataGenerator(
-        rescale=1. / 255
-    )
+    pass
