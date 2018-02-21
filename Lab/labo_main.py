6088a74490b8bf5bc43f081394e50c973ead0207
0rigine <malo.puissant.20160347@efrei.net>
2018-02-21 18:24:37 +0100

    Lab package init

---
+++ Lab/labo_main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# ---- Description ----
+""" Lab main script of project """
+
+# ---- Imports ----
+
+# ---- Script ----
+if __name__ == '__main__':
+    pass
