77f813c8aaf3f137efb474a476066df9393cc573
0rigine <malo.puissant.20160347@efrei.net>
2018-03-06 19:06:42 +0100

    Room_Agent added

---
+++ virtual_room/Room_Agent.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# ---- Description ----
+""" An agent wich explore the room and try to map it """
+
+
+# ---- Imports ----
+
+
+# ---- Class ----
+class Room_Agent:
+    """ An agent wich explore the room and try to map it """
+    general_id = 0
+
+    def __init__(self):
+        self.id = Room_Agent.general_id
+        Room_Agent.general_id += 1
