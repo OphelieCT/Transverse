a3aec979a8c724706e660f20b1eb0fd112061c4c
0rigine <malo.puissant.20160347@efrei.net>
2018-03-06 19:01:20 +0100

    Virtual Room constructor

--- virtual_room/Room.py
+++ virtual_room/Room.py
@@ -2,9 +2,23 @@
 # coding: utf-8
 
 # ---- Description ----
-""" Description """
+""" A virtual room where we can test every algorithms """
 
 # ---- Imports ----
+import numpy as np
+
+# ---- Some setups ----
+np.random.seed()
 
 
-# ---- Script ----
+# ---- Class ----
+class Virtual_Room:
+    """ A virtual room to experiment some tests """
+
+    def __init__(self, _dimensions: tuple):
+        self.dimensions = _dimensions
+        self.grid = None
+        self.create_grid()
+
+    def create_grid(self):
+        self.grid = np.random.randint(0, 1, size=self.dimensions)
