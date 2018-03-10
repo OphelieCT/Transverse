93080923827b59bed1eaaa41548f3f5bfeec2e78
0rigine <malo.puissant.20160347@efrei.net>
2018-03-10 14:48:00 +0100

    Mise en place du choix de direction A venir : Zone sélectionnée plus large pour choix de direction

--- main.py
+++ main.py
@@ -6,8 +6,19 @@
 
 # ---- Imports ----
 import virtual_room
+import matplotlib.pyplot as plt
 
 # ---- Script ----
 if __name__ == '__main__':
-    room = virtual_room.Virtual_Room((3, 3))
+    room = virtual_room.Virtual_Room((1000, 1000))
     agent = virtual_room.Artificial_Agent(_map=room.grid)
+    print('Score de départ : {}'.format(agent.score))
+    turns = 10 ** 4
+    for i in range(turns):
+        agent.move()
+        agent.collect()
+        if i % (turns / 10) == 0:
+            print('{}) - Score : {}'.format(i, agent.score))
+    print('Score final : {}'.format(agent.score))
+    plt.scatter(agent.movement_history.get('x'), agent.movement_history.get('y'))
+    plt.show()
--- virtual_room/Convolutional_Agent.py
+++ virtual_room/Convolutional_Agent.py
@@ -12,8 +12,7 @@ import numpy as np
 import copy
 import keras.backend as K
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
-from keras.preprocessing.image import array_to_img, img_to_array
+from keras.layers import Dropout, Dense
 from keras import losses
 
 
@@ -21,9 +20,14 @@ from keras import losses
 class Artificial_Agent(virtual_room.Room_Agent):
     """ Room Agent with artificial intelligence control """
 
-    def __init__(self, shape=(224, 224), _map=None, network=None, depth=1):
+    def __init__(self, shape=(224, 224), _map=None, network=None, depth=1, save_file='test.h5', base_position=None):
         virtual_room.Room_Agent.__init__(self, _map)
+        self.position = base_position
+        if base_position is None:
+            self.position = (len(self.map) // 2, len(self.map[0]) // 2)
         self.score = 0
+        self.movement_history = {'x': [], 'y': []}
+        self.save_file = save_file
         self.net = network
         self.traductor = {
             (0, 0, 0, 1): [-1, 0],  # haut
@@ -36,68 +40,66 @@ class Artificial_Agent(virtual_room.Room_Agent):
             self.shape = (depth,) + shape
         if self.net is None:
             self.build_net()
-            self.train_to_move()
+            try:
+                self.net.load_weights(save_file)
+            except OSError:
+                self.train_to_move()
 
     def build_net(self):
-        self.net = Sequential()
-        self.net.add(Conv2D(20, (3, 3), input_shape=self.shape, activation='relu'))
-        self.net.add(MaxPooling2D())
-        self.net.add(Dropout(0.1))
-        self.net.add(Conv2D(20, (3, 3), activation='relu'))
-        self.net.add(MaxPooling2D())
-        self.net.add(Flatten())
-        self.net.add(Dense(256, activation='relu'))
-        self.net.add(Dropout(0.1))
-        self.net.add(Dense(4, activation='softmax'))
+        self.net = Sequential([
+            Dense(20, input_dim=4, activation='relu'),
+            Dropout(0.2),
+            Dense(40, activation='relu'),
+            Dropout(0.2),
+            Dense(4, activation='softmax')
+        ])
         self.net.compile(optimizer='adadelta', loss=losses.binary_crossentropy, metrics=['accuracy'])
 
-    def prepare_map(self, map_to_convert, pos_x=None, pos_y=None):
-        plan = copy.copy(np.array(map_to_convert))
+    def prepare_batch(self, pos_x=None, pos_y=None):
         if pos_x is None:
             pos_x = self.position[0]
         if pos_y is None:
             pos_y = self.position[1]
-        plan[pos_x][pos_y] = 0.5
-        if K.image_data_format() == 'channels_first':
-            dimensions = self.shape[1:]  # prepare dimensions to resize after
-            plan = plan.reshape((self.shape[0],) + plan.shape)  # prepare to image conversion
-        else:
-            dimensions = self.shape[:-1]  # prepare dimensions to resize after
-            plan = plan.reshape(plan.shape + (self.shape[-1],))  # prepare to image conversion
-        plan = array_to_img(plan)
-        plan = plan.resize(dimensions)
-        plan.save('test.jpg')
-        return img_to_array(plan)
+        temp = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i ** 2 != j ** 2:
+                    try:
+                        temp.append(self.map[pos_x + i][pos_y + j])
+                    except IndexError:
+                        temp.append(1.)
+        return temp
 
     def choose_action(self):
         """ Movement {0,1,2,3} -> {haut, droite, bas, gauche} """
-        plan = self.prepare_map(self.map)
+        plan = self.prepare_batch()
         prediction = self.net.predict(np.array([plan]))[0]
         return prediction
 
     def train_to_move(self):
         training_map = np.full(self.map.shape, 0.)
         x_train = np.array([
-            self.prepare_map(training_map, 0, (len(training_map[0]) - 1) // 2),  # milieu haut
-            self.prepare_map(training_map, (len(training_map) - 1) // 2, 0),  # milieu gauche
-            self.prepare_map(training_map, len(training_map) - 1, (len(training_map[0]) - 1) // 2),  # milieu bas
-            self.prepare_map(training_map, (len(training_map) - 1) // 2, len(training_map[0]) - 1)  # milieu droite
+            (0, 0, 0, 1),  # milieu haut
+            (0, 0, 1, 0),  # milieu droite
+            (0, 1, 0, 0),  # milieu bas
+            (1, 0, 0, 0)  # milieu gauche
         ])
         predictions = self.net.predict(x_train)
-        print(np.round(predictions), '\n')
         x_targets = np.array([
             (0, 1, 0, 0),  # bas
-            (0, 0, 1, 0),  # droite
+            (1, 0, 0, 0),  # gauche
             (0, 0, 0, 1),  # haut
-            (1, 0, 0, 0)  # gauche
+            (0, 0, 1, 0),  # droite
         ])
-        history = self.net.fit(x=x_train, y=x_targets, epochs=10, batch_size=1, verbose=0)
-        print(np.round(self.net.predict(x_train)))
+        history = self.net.fit(x=x_train, y=x_targets, epochs=1000, batch_size=1, verbose=0)
+        self.net.save_weights(self.save_file)
 
     def move(self, next_x=None, next_y=None):
         predictions = tuple(np.round(self.choose_action()))
         next_x, next_y = np.array(self.position) + np.array(self.traductor.get(predictions))
-        movement = virtual_room.Room_Agent.move(self, next_x, next_y)
+        if virtual_room.Room_Agent.move(self, next_x, next_y) == 0:
+            self.movement_history['x'].append(next_x)
+            self.movement_history['y'].append(next_y)
 
     def collect(self):
         self.score += self.map[self.position[0]][self.position[1]]
