d5b0a0d8f9f56d1a83eba6596f5b7f919775aef3
0rigine <malo.puissant.20160347@efrei.net>
2018-03-11 22:14:49 +0100

    Threading class

--- virtual_room/artificial_coach.py
+++ virtual_room/artificial_coach.py
@@ -4,14 +4,19 @@
 # ---- Description ----
 """ Class wich trains artificial agents and let them evolve """
 
-
 # ---- Imports ----
+import threading
+
+# ---- Settings ----
+lock = threading.RLock()
+
 
 # ---- Class ----
-class Articial_Coach:
+class Articial_Coach(threading.Thread):
     """ Trainer for artificial agent population """
 
     def __init__(self, own_map, population_size, mutation_rate=30):
+        threading.Thread.__init__(self)
         self.pop_size = population_size
         self.mutation_rate = mutation_rate
         self.map = own_map
