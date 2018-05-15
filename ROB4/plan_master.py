de014086542078732bbd961c5977b42d3816e5eb
0rigine <malo.puissant.20160347@efrei.net>
2018-05-15 11:00:31 +0200

    Plan Manager

--- ROB4/plan_master.py
+++ ROB4/plan_master.py
@@ -18,6 +18,125 @@ class Plan_Master:
     unknown = -1
 
     def __init__(self, position=(0, 0), direction=0, shape=(1, 1)):
-        self.plan = np.full(shape, Plan_Master.freeway).tolist()
+        self.plan: list = np.full(shape, Plan_Master.freeway).tolist()
         self.position = position
         self.direction = direction
+
+    def save_plan(self, filename):
+        img = plt.figure()
+        fig = plt.imshow(np.array(self.plan), interpolation='nearest')
+        fig.set_cmap('hot')
+        plt.axis('off')
+        img.savefig(filename, bbox_inches='tight', pad_inches=0, transparent=True)
+
+    def show_plan(self):
+        fig = plt.imshow(np.array(self.plan), interpolation='nearest')
+        fig.set_cmap('hot')
+        plt.axis('off')
+        plt.show()
+
+    def place_mesures(self, datas):
+        batch = []
+        datas = copy.deepcopy(datas)
+        for info in datas:
+            batch.append(info)
+            batch[-1][1] = (batch[-1][1] + 360 - self.direction) % 360  # if robot add its direction to mesures
+        for point in batch:
+            self.position = self.add_point(point[0], point[1])
+
+    def put_point(self, x, y, code):
+        self.plan[x][y] = code
+
+    def add_point(self, distance, angle):
+        relative_x, relative_y = self.calculate_relative(distance, angle)
+
+        new_x = self.position[0]
+        new_y = self.position[1]
+
+        point_x = new_x + relative_x
+        point_y = new_y + relative_y
+
+        if point_x >= len(self.plan):
+            for i in range(len(self.plan), point_x + 1):
+                self.plan.append([0] * len(self.plan[0]))
+        elif point_x < 0:
+            for i in range(point_x, 0):
+                self.plan.insert(0, [0] * len(self.plan[0]))
+            new_x += np.absolute(point_x)
+
+        if point_y < 0:
+            y = np.absolute(point_y)
+            for i in range(len(self.plan)):
+                self.plan[i] = [0] * y + self.plan[i]
+            new_y += np.absolute(point_y)
+        elif point_y >= len(self.plan[0]):
+            length = point_y - len(self.plan[0])
+            for i in range(len(self.plan)):
+                self.plan[i] += [0] * (length + 1)
+
+        point_x = max(0, point_x)
+        point_y = max(0, point_y)
+        self.put_point(point_x, point_y, 1)
+        return new_x, new_y
+
+    def search_pattern(self, datas):
+        positions = []
+        for point in datas:
+            positions.append(self.calculate_relative(point[0], point[1]))
+        for i in range(len(self.plan)):
+            for j in range(len(self.plan[i])):
+                if self.plan[i][j] not in (0, 5):
+                    continue
+                valid = 0
+                for x, y in positions:
+                    try:
+                        if self.plan[i + x][j + y] == 0:
+                            break
+                        else:
+                            valid += 1
+                    except IndexError:
+                        break
+                if valid == len(positions):
+                    return i, j
+
+    def scan(self, rangemin, rangemax, datas, zero_test=True):
+        datas = copy.deepcopy(datas)
+        pred = []
+        for i in range(rangemin, rangemax):
+            tests = []
+            for index in datas:
+                tests.append(copy.deepcopy(index))
+                tests[-1][1] = (tests[-1][1] + 360 - i) % 360
+            coord = self.search_pattern(tests)
+            if coord is not None:
+                pred.append((coord, i))
+                if zero_test:
+                    if i == 0:
+                        return self.scan(-20, 20, datas, zero_test=False)
+        return pred
+
+    def search_position(self, datas):
+        datas = copy.deepcopy(datas)
+        pred = self.scan(0, 360, datas, zero_test=True)
+        resdir = 0
+        coord = np.full(shape=2, fill_value=0)
+        for prediction in pred:
+            coord += np.array(prediction[0])
+            resdir += prediction[1]
+
+        try:
+            resdir /= len(pred)
+            coord = coord // len(pred)
+            coord = coord.tolist()
+        except ZeroDivisionError:
+            resdir = None
+            coord = None
+        if resdir < 0:
+            resdir = (360 + resdir) % 360
+        return coord, resdir
+
+    @staticmethod
+    def calculate_relative(distance, angle):
+        relative_x = int(np.round(np.sin(np.deg2rad(angle)) * distance))
+        relative_y = int(np.round(np.cos(np.deg2rad(angle)) * distance))
+        return relative_x, relative_y
