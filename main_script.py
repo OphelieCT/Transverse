5fddf52d04d1e40d94857fffb91d56cfb9090b11
0rigine <malo.puissant.20160347@efrei.net>
2018-05-22 15:46:13 +0200

    Project closed [finished]

---
+++ main_script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# ---- Description ----
+""" Main script of project """
+
+# ---- Imports ----
+import ROB4
+import time
+
+# ---- Script ----
+if __name__ == '__main__':
+    rob = ROB4.Rob(name='Jammy')
+    while True:
+        rob.upgrade_plan(2)
+        rob.save_plan('/media/usb0/plan.png')
+        time.sleep(10)
