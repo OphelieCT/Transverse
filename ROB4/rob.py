590a303573d58381634dc237556ca0a899a1ad59
0rigine <malo.puissant.20160347@efrei.net>
2018-05-24 09:08:24 +0200

    Com fix

--- ROB4/rob.py
+++ ROB4/rob.py
@@ -29,9 +29,10 @@ class Rob(
         """ Get measures per lines from arduino board """
         measures = []
         data = ""
-        self.send_permission(permission='measure')
+        self.send_permission(permission='measure', waitfor='measure')
         while "EOF" not in data:
             data = self.receive_data_line().decode()
+            print("Datas :", data)
             if len(data) > 1 and self.ending_msg not in data:
                 data = data.split(' ')
                 temp = {'distance': np.round(float(data[0]), 0), 'angle': int(float(data[1]))}
@@ -70,26 +71,31 @@ class Rob(
     def listen(self):
         while True:
             msg = self.receive_data_line().decode()
+            print(msg)
             if 'left' in msg:
+                self.send_permission(waitfor='left')
                 self.direction += 360 - 90
                 self.direction %= 360
             elif 'right' in msg:
+                self.send_permission(waitfor='right')
                 self.direction += 90
                 self.direction %= 360
             elif 'position' in msg:
+                self.send_permission(waitfor='position')
                 self.find_self()
             elif 'plan' in msg:
+                self.send_permission(waitfor='plan')
                 self.update_plan()
             else:
                 time.sleep(1)
                 continue
             break
 
-    def send_permission(self, permission='launch'):
+    def send_permission(self, permission='launch', waitfor='permission_needed'):
         msg = self.receive_data_line()
-        while "permission_needed" not in msg.decode():
+        while waitfor not in msg.decode():
             self.send_data(permission)
             msg = self.receive_data_line()
-        while "permission_needed" in msg.decode():
+        while "received" not in msg.decode():
             self.send_data(permission)
             msg = self.receive_data_line()
