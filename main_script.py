34b5213532ce75b695f43620014043930fd30ba1
0rigine <malo.puissant.20160347@efrei.net>
2018-05-24 03:33:28 +0200

    Switch var

--- main_script.py
+++ main_script.py
@@ -10,11 +10,11 @@ import os
 
 # ---- Script ----
 if __name__ == '__main__':
-    rob = ROB4.Rob(name='Jammy')
     mnt_point = '/media/usb0/'
     plan_name = 'plan.png'
     plan_path = os.path.join(mnt_point, plan_name)
     ROB4.automount(mnt_point, FLAG='Transverse')
+    rob = ROB4.Rob(name='Jammy')
     while True:
         rob.listen()
         rob.save_plan(plan_path)
